1f242e6a8239d36338a8b73c43dd94ce6ae7e20f
daodao-windows <515144382@qq.com>
2018-06-12 12:23:31 +0800

    重构了五角星代码

--- 五角星/main.js
+++ 五角星/main.js
@@ -25,26 +25,73 @@ canvas.height = 800
 
 
 //画一片星星
-function fivePointStar(ctx, x, y, R, r, rotate){ 
+// function fivePointStar(ctx, x, y, R, r, rotate){ 
 
-    ctx.fillStyle = 'yellow'
+//     ctx.fillStyle = 'yellow'
+//     ctx.lineWidth = 3
+//     ctx.beginPath()
+//     for(let i = 0;i<5;i++){
+//         ctx.lineTo(x + R*Math.cos(Math.PI/180*(18+ 72*i + rotate)), y - R*Math.sin(Math.PI/180*(18 + 72*i + rotate)))
+//         ctx.lineTo(x + r*Math.cos(Math.PI/180*(54+ 72*i + rotate)), y - r*Math.sin(Math.PI/180*(54 + 72*i + rotate)))
+//     }
+//     ctx.closePath()
+//     ctx.fill()
+
+// }
+
+// context.fillStyle = 'black'
+// context.fillRect(0, 0, canvas.width, canvas.height)
+
+
+// for(let i = 0; i<150 ; i++){
+//     var x = Math.random()
+//     var R = Math.random()*10 + 15
+//     var x = Math.random()*canvas.width
+//     var y = Math.random()*canvas.height
+//     fivePointStar(context, x, y, R, R/2, 30)
+// }
+
+
+// 重构
+
+context.fillStyle = 'black'
+context.fillRect(0, 0, canvas.width, canvas.height)
+
+
+function drawStar(ctx, x, y, r, rotate){
+
+    ctx.save()
+    ctx.translate(x, y)
+    ctx.rotate( Math.PI/180 * rotate)
+
+    fivePointStar(ctx)
+    // 先绘制标准的五角星，然后根据变形绘制x, y偏移量，大小为r， 旋转角度为rotate的五角星
+    ctx.fillStyle = '#fb3'
+    ctx.strokeStyle = '#fd5'
+    ctx.lineWidth = 3
+    ctx.lineJoin = 'round'
+    ctx.fill()
+    ctx.stroke()
+
+    ctx.restore()
+}
+
+//标准的五角星
+function fivePointStar(ctx){ 
     ctx.lineWidth = 3
     ctx.beginPath()
     for(let i = 0;i<5;i++){
-        ctx.lineTo(x + R*Math.cos(Math.PI/180*(18+ 72*i + rotate)), y - R*Math.sin(Math.PI/180*(18 + 72*i + rotate)))
-        ctx.lineTo(x + r*Math.cos(Math.PI/180*(54+ 72*i + rotate)), y - r*Math.sin(Math.PI/180*(54 + 72*i + rotate)))
+        ctx.lineTo(20*Math.cos(Math.PI/180*(18+ 72*i)), -20*Math.sin(Math.PI/180*(18 + 72*i )))
+        ctx.lineTo(10*Math.cos(Math.PI/180*(54+ 72*i)), -10*Math.sin(Math.PI/180*(54 + 72*i )))
     }
     ctx.closePath()
-    ctx.fill()
-
 }
 
 
-for(let i = 0; i<150 ; i++){
+for(let i = 0; i<100 ; i++){
     var x = Math.random()
-    var R = Math.random()*10 + 15
+    var r = Math.random()*10 + 15
     var x = Math.random()*canvas.width
     var y = Math.random()*canvas.height
-
-fivePointStar(context, x, y, R, R/2, 30)
+    drawStar(context, x, y, r, 30)
 }
