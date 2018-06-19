0de314fd98d48b509c03ce85cdd591099d0db77b
daodao-windows <515144382@qq.com>
2018-06-19 17:04:10 +0800

    新增放大镜demo框架

---
+++ demo_模板/main.js
@@ -0,0 +1,8 @@
+
+const canvas = document.getElementById('drawing')
+const context = canvas.getContext('2d')
+
+window.onload = ()=>{
+    canvas.width = 1200
+    canvas.height = 800
+}
---
+++ 放大镜效果demo/main.js
@@ -0,0 +1,12 @@
+
+const canvas = document.getElementById('drawing')
+const context = canvas.getContext('2d')
+var image = new Image()
+
+window.onload = ()=>{
+    canvas.width = 1200
+    canvas.height = 800
+
+    image.src = ''
+    context.drawImage()
+}
