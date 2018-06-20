53e2863b84a39657c41435566c4a5d34139eea0a
daodao-windows <515144382@qq.com>
2018-06-20 16:29:43 +0800

    增加模糊和马赛克的滤镜

--- 图片像素处理demo/main.js
+++ 图片像素处理demo/main.js
@@ -88,7 +88,73 @@ function blurFilter(){
     var templateImageData = beforeContext.getImageData(0, 0, beforeCanvas.width, beforeCanvas.height)
     var templatePixelData = templateImageData.data
    
+    //调节模糊半径 弊端：半径一大，边缘无法模糊
+    var blurR=1
 
+    for(let i=blurR; i<=beforeCanvas.height -blurR; i++){
+        for(let j=blurR; j<=beforeCanvas.width -blurR; j++){
+            var totalr =0
+            var totalg =0
+            var totalb =0
+            for(let a=-blurR;a<=blurR;a++){
+                for(let b=-blurR; b<=blurR; b++){
+                    var p = (i+a)*beforeCanvas.width + j+b
+                    totalr += templatePixelData[4*p + 0]
+                    totalg += templatePixelData[4*p + 1]
+                    totalb += templatePixelData[4*p + 2]
+                }
+            }
+            pixelData[4*(i*beforeCanvas.width+j)+0] = totalr / ((2*blurR+1)*(2*blurR+1))
+            pixelData[4*(i*beforeCanvas.width+j)+1] = totalg / ((2*blurR+1)*(2*blurR+1))
+            pixelData[4*(i*beforeCanvas.width+j)+2] = totalb / ((2*blurR+1)*(2*blurR+1))
+        }
+    }
+
+
+    afterContext.putImageData(imageData, 0, 0, 0, 0, afterCanvas.width, afterCanvas.height)
+}
+
+
+
+//马赛克效果
+function mosaicFilter(){
+    var imageData = beforeContext.getImageData(0, 0, beforeCanvas.width, beforeCanvas.height)
+    var pixelData = imageData.data
+
+    var templateImageData = beforeContext.getImageData(0, 0, beforeCanvas.width, beforeCanvas.height)
+    var templatePixelData = templateImageData.data
+   
+    //调节模糊半径 弊端：半径一大，边缘无法模糊
+    var sizex = 8
+    var sizey = 8
+
+    for(let i=1; i<=beforeCanvas.height -sizey; i+=sizey){
+        for(let j=1; j<=beforeCanvas.width -sizex; j+=sizex){
+            var totalr =0
+            var totalg =0
+            var totalb =0
+            for(let a=1;a<=sizey;a++){
+                for(let b=1; b<=sizex; b++){
+                    var p = (i+a)*beforeCanvas.width + (j+b)
+                    totalr += templatePixelData[4*p + 0]
+                    totalg += templatePixelData[4*p + 1]
+                    totalb += templatePixelData[4*p + 2]
+                }
+            }
+           var averageR = totalr/(sizex*sizey)
+           var averageG = totalg/(sizex*sizey)
+           var averageB = totalb/(sizex*sizey) 
+
+           for(let a=1;a<sizey; a++){
+               for(let b=1;b<sizex;b++){
+                    var p = (i+a)*beforeCanvas.width + (j+b)
+                    pixelData[4*p + 0] = averageR
+                    pixelData[4*p + 1] = averageG
+                    pixelData[4*p + 2] = averageB
+               }
+           }
+        }
+    }
 
 
     afterContext.putImageData(imageData, 0, 0, 0, 0, afterCanvas.width, afterCanvas.height)
